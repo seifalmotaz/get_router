b61e92b90cb4b04dd604ddca6249e6815c257459
Seif Almotaz <seifalmotaz@outlook.com>
2024-06-06 16:47:13 +0300

    .

---
+++ bin/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io' as io;
+
+void main(List<String> args) {
+  list
+  // 1. Read all files inside /lib/
+  io.Directory.fromUri("uri").list();
+  // 2. filter files that end with .r.dart
+  list.where
+  // 3. decode json for the line that start with ">>"
+  for loop 
+  read string
+  split line "\n"
+  first line that starts with "// >>"
+  extract the rest of the line
+  var i = "".replaceFirst("// >>", "");
+ list .add decode(i)
+  // 4. collect all jsons to one list
+  // 5. generate from that list the list of GetPage
+
+  
+}
